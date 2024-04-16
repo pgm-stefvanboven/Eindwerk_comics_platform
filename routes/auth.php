adb35f1b39a331004fc15cc5279bdff993ad5c86
Stef Van Boven <stefvboven2020@outlook.com>
2024-04-16 21:27:13 +0200

    added authentication the user cannot access the homepage unless the user logs in or registers I have secured and shielded the route to the homepage

--- routes/auth.php
+++ routes/auth.php
@@ -11,28 +11,19 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login')
+            ->middleware('guest');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+Route::post('login', [AuthenticatedSessionController::class, 'store'])
+            ->middleware('guest');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+Route::middleware('auth')->group(function () {
+    // Andere auth routes hier
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.store');
+    Route::get('index', function () {
+        return view('index');
+    })->name('index');
 });
 
 Route::middleware('auth')->group(function () {
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-    // return view('welcome');
-});
+})->middleware(['auth', 'verified'])
+  ->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])
+  ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,4 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
